7f6a3283f9d0c87f6d5d7935cd636e1f03bbe1b6
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 09:06:16 -0400

    PORT 8080 and 8081 enable

--- src/main/java/com/example/demo/auth/CorsConfig.java
+++ src/main/java/com/example/demo/auth/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry)
     {
         registry.addMapping("/**")  // Apply CORS to all paths
-                .allowedOrigins("https://professional-practices-system.vercel.app","https://professional-practices-system.vercel.app/login")
+                .allowedOrigins("https://professional-practices-system.vercel.app","https://professional-practices-system.vercel.app/login", "http://localhost:8080","http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
