d72edac13f85380002748074f26e0697edbe7dbf
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 00:24:37 -0400

    Welcome to REST API in Java message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.interfaces.MessageResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import com.example.demo.interfaces.MessageResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public ResponseEntity<MessageResponse> welcomeMessage(){
+        return ResponseEntity.ok(new MessageResponse("Welcome to Luis Somoza PPS REST API In Java"));
+    }
+}
