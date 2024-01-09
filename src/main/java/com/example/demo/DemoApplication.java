46a339af0db193d228515883a22d9e037c2a32e0
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 01:25:33 -0400

    External Database Conection Testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,6 @@ public class DemoApplication {
 
 	public String PORT = System.getenv("PORT");
 	public String DATABASE_URL = System.getenv("DATABASE_URL");
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
