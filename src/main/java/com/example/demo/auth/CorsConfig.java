9d7b39355349fb4c6f363d3a4181e9990a2b9086
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 15:38:26 -0400

    Change in docker compiler

--- src/main/java/com/example/demo/auth/CorsConfig.java
+++ src/main/java/com/example/demo/auth/CorsConfig.java
@@ -1,1 +1,39 @@
 package com.example.demo.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**");
+    }
+
+
+}
+
+    /*
+
+        @Bean
+        public CorsFilter corsFilter()
+
+        {
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200","https://professional-practices-system-k7mn97rom-lu27656348.vercel.app","https://professional-practices-system.vercel.app","https://professional-practices-system.vercel.app/login"));
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+            corsConfiguration.setAllowedHeaders(List.of("*"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfiguration);
+
+            return new CorsFilter(source);
+        }
+    */
