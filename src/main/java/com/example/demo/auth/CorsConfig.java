b32f39b67ec873478f03cbdfaadfa1c6347a3521
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 08:23:13 -0400

    New addon to Cors Mapping

--- src/main/java/com/example/demo/auth/CorsConfig.java
+++ src/main/java/com/example/demo/auth/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://professional-practices-system-k7mn97rom-lu27656348.vercel.app/login")
+                        .allowedOrigins("http://localhost:4200", "https://professional-practices-system-k7mn97rom-lu27656348.vercel.app/login","https://professional-practices-system-k7mn97rom-lu27656348.vercel.app")
                         .allowedMethods(
                                 HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
