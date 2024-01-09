9a5bb6a621ba778e94b6cdcf46f635e1fa323266
Lu27656348 <luiscarlossomoza@gmail.com>
2024-01-09 01:40:57 -0400

    Docker File Updated

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class MainController {
-    @GetMapping
+    @GetMapping({"/","/home"})
     public ResponseEntity<MessageResponse> welcomeMessage(){
         return ResponseEntity.ok(new MessageResponse("Welcome to Luis Somoza PPS REST API In Java, have a good time <3"));
     }
